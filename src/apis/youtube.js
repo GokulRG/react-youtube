00e7f09f4f8fedabd416583ac057ea73726342db
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-15 23:12:50 -0700

    Adding axios support
    Adding axios support and cleaning up the access key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
-const ACCESS_KEY = 'AIzaSyDNv5WJjj0HeQNyf06wu80xKwuv5TkpP5U';
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    options: {
+        params: {
+            required: 'snippet',
+            maxResults: 5,
+            key: 'AIzaSyDNv5WJjj0HeQNyf06wu80xKwuv5TkpP5U'
+        }
+    }
+});
