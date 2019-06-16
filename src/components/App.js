3195cb9bae35fc0e6f3f18c02bede3c29ed2a966
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-15 23:40:34 -0700

    Removing youtube.js
    Removing youtube.js because the params object in the file is overridden by the one in App.js essentially just passing in 1 parameter instead of all the defined ones. So moving them all to app.js.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import youtube from '../apis/youtube';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
+    onTermSubmit = async searchTerm => {
+        //It's called q -- query because that's what youtube has defined it to be
+        youtube.get('https://www.googleapis.com/youtube/v3/search', {
+            params: {
+                q: searchTerm,
+                part: 'snippet',
+                maxResults: 5,
+                key: 'AIzaSyDNv5WJjj0HeQNyf06wu80xKwuv5TkpP5U'
+            }
+        });
+    }
+
     render() {
         return (
             <div className="ui container">
-                <SearchBar/>
+                <SearchBar onTermSubmit={this.onTermSubmit}/>
             </div>
         );
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,13 +6,12 @@ class SearchBar extends React.Component {
     state = {searchTerm : ''};
 
     onInputChange = event => {
-        console.log(event.target.value);
         this.setState({searchTerm : event.target.value});
     }
 
     onFormSubmit = event => {
         event.preventDefault();
-        //TODO: Add callback method from Parent Component
+        this.props.onTermSubmit(this.state.searchTerm);
     }
 
     render() {
