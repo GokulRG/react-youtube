65eb23254f351e7d405c5b9926ed3e2571660251
Gokul Kramnik <ramakrishnan.go@husky.neu.edu>
2019-06-17 21:12:03 -0700

    UI Finetune
    Adding UI/JSX/CSS changes and implementing the Semantic UI Grid system to make the UI more appealing

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,15 +2,24 @@ import React from 'react';
 
 const VideoDetail = ({ video }) => {
 
-    if (video === null) {
-        return <div/>;
-    } else {
-        return (
-            <div>
-                {video.snippet.title}
-            </div>
-        );
+    if (!video) {
+        return <div />;
     }
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+    return (
+        <div>
+            <div className="ui embed">
+                <iframe src={videoSrc} title={video.snippet.description}/>
+            </div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
+        </div>
+    );
 };
+
 
 export default VideoDetail;
